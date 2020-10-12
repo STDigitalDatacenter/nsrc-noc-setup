fc0340467ef250f5a5ac3528d4911997ed1ff9f0
Brian Candler <brian@nsrc.org>
2020-10-12 20:40:19 +0100

    WIP: Netbox data population (hostX VMs)

--- netbox/populate.py
+++ netbox/populate.py
@@ -8,7 +8,7 @@ from dcim.choices import InterfaceTypeChoices, PortTypeChoices, PowerPortTypeCho
 def make(klass, filter, **defaults):
     instance, created = klass.objects.get_or_create(defaults=defaults, **filter)
     if not created:
-        for attr, value in defaults.items(): 
+        for attr, value in defaults.items():
             setattr(instance, attr, value)
         instance.save()
     return instance
@@ -123,63 +123,87 @@ for i in range(1,7):
     device[i] = {}
     # Core rack
     device[i]["pp-c1"] = make(Device, dict(site=s, name="pp-c1"),
-                              name="pp-c1", rack=rack[i]["c1"], position=42, face="front",
+                              rack=rack[i]["c1"], position=42, face="front",
                               device_type=device_type["patch_fibre_12"],
                               device_role=device_role["patching"])
     device[i]["bdr1"] = make(Device, dict(site=s, name="bdr1"),
-                              name="bdr1", rack=rack[i]["c1"], position=40, face="front",
+                              rack=rack[i]["c1"], position=40, face="front",
                               device_type=device_type["iosv"],
                               device_role=device_role["border_router"],
                               platform=platform["ios_xe"])
     device[i]["core1"] = make(Device, dict(site=s, name="core1"),
-                              name="core1", rack=rack[i]["c1"], position=39, face="front",
+                              rack=rack[i]["c1"], position=39, face="front",
                               device_type=device_type["iosvl2"],
                               device_role=device_role["core_router"],
                               platform=platform["ios_xe"])
     device[i]["srv1"] = make(Device, dict(site=s, name="srv1"),
-                              name="srv1", rack=rack[i]["c1"], position=37, face="front",
+                              rack=rack[i]["c1"], position=37, face="front",
                               device_type=device_type["generic_2u_server"],
                               device_role=device_role["monitoring"],
                               platform=platform["ubuntu"])
     # Building 1
     device[i]["pp-b1-1"] = make(Device, dict(site=s, name="pp-b1-1"),
-                              name="pp-b1-1", rack=rack[i]["b1-1"], position=6, face="front",
+                              rack=rack[i]["b1-1"], position=6, face="front",
                               device_type=device_type["patch_fibre_12"],
                               device_role=device_role["patching"])
     device[i]["dist1-b1"] = make(Device, dict(site=s, name="dist1-b1"),
-                              name="dist1-b1", rack=rack[i]["b1-1"], position=5, face="front",
+                              rack=rack[i]["b1-1"], position=5, face="front",
                               device_type=device_type["iosvl2"],
                               device_role=device_role["distribution_switch"],
                               platform=platform["ios_xe"])
     device[i]["edge1-b1"] = make(Device, dict(site=s, name="edge1-b1"),
-                              name="edge1-b1", rack=rack[i]["b1-1"], position=4, face="front",
+                              rack=rack[i]["b1-1"], position=4, face="front",
                               device_type=device_type["iosvl2"],
                               device_role=device_role["edge_switch"],
                               platform=platform["ios_xe"])
     device[i]["edge2-b1"] = make(Device, dict(site=s, name="edge2-b1"),
-                              name="edge2-b1", rack=rack[i]["b1-1"], position=3, face="front",
+                              rack=rack[i]["b1-1"], position=3, face="front",
                               device_type=device_type["iosvl2"],
                               device_role=device_role["edge_switch"],
                               platform=platform["ios_xe"])
     # Building 2
     device[i]["pp-b2-1"] = make(Device, dict(site=s, name="pp-b2-1"),
-                              name="pp-b2-1", rack=rack[i]["b2-1"], position=6, face="front",
+                              rack=rack[i]["b2-1"], position=6, face="front",
                               device_type=device_type["patch_fibre_12"],
                               device_role=device_role["patching"])
     device[i]["dist1-b2"] = make(Device, dict(site=s, name="dist1-b2"),
-                              name="dist1-b2", rack=rack[i]["b2-1"], position=5, face="front",
+                              rack=rack[i]["b2-1"], position=5, face="front",
                               device_type=device_type["iosvl2"],
                               device_role=device_role["distribution_switch"],
                               platform=platform["ios_xe"])
     device[i]["edge1-b2"] = make(Device, dict(site=s, name="edge1-b2"),
-                              name="edge1-b2", rack=rack[i]["b2-1"], position=4, face="front",
+                              rack=rack[i]["b2-1"], position=4, face="front",
                               device_type=device_type["iosvl2"],
                               device_role=device_role["edge_switch"],
                               platform=platform["ios_xe"])
     device[i]["edge2-b2"] = make(Device, dict(site=s, name="edge2-b2"),
-                              name="edge2-b2", rack=rack[i]["b2-1"], position=3, face="front",
+                              rack=rack[i]["b2-1"], position=3, face="front",
                               device_type=device_type["iosvl2"],
                               device_role=device_role["edge_switch"],
                               platform=platform["ios_xe"])
+
+## Use virtualization model for hostX
+lxd = make(ClusterType, dict(slug="lxd"), name="lxd", description="lxd containers")
+for i in range(1,7):
+    print("--- Campus %d ---" % i)
+    for j in range(1,7):
+        cluster = make(Cluster, dict(name="campus%d-cluster1" % i),
+                       type=lxd, site=site[i])
+        srv1 = device[i]["srv1"]
+        srv1.cluster = cluster
+        srv1.save()
+        vm = make(VirtualMachine, dict(cluster=cluster, name="host%d" % j),
+                  platform=platform["ubuntu"], role=device_role["monitoring"])
+        eth0 = make(VMInterface, dict(virtual_machine=vm, name="eth0"))
+        ip4 = make(IPAddress, dict(address="100.68.%d.%d/28" % (i, 130+j)),
+                   assigned_object=eth0, dns_name="eth0.host%d.campus%d.ws.nsrc.org" % (j, i))
+        ip6 = make(IPAddress, dict(address="2001:DB8:%d:1::%d/64" % (i, 130+j)),
+                   assigned_object=eth0, dns_name="eth0.host%d.campus%d.ws.nsrc.org" % (j, i))
+        vm.primary_ip4 = ip4
+        vm.primary_ip6 = ip6
+        vm.save()
+        eth1 = make(VMInterface, dict(virtual_machine=vm, name="eth1"))
+        ip4 = make(IPAddress, dict(address="100.64.0.%d/22" % (i*10 + j)),
+                   assigned_object=eth1, dns_name="eth1.host%d.campus%d.ws.nsrc.org" % (j, i))
 
 # End
